0cf3e7624de0a1adb3d1e831431f5bb7cde1a51e
Alex Avila <alex.ramon2004@gmail.com>
2025-07-07 05:21:15 -0500

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
 
   //http://localhost:8080/api/v1
   production: false,
-  serverBasePath: 'http://localhost:8080/api/v1',
-  prodBasePath: 'http://localhost:8080/api/v1'
+  serverBasePath: 'https://inncontrol-aws-api.llantatech.org.pe/api/v1',
+  prodBasePath: 'https://inncontrol-aws-api.llantatech.org.pe/api/v1'
   //https://inncontrol-api.ryzeon.me/api/v1
 };
